627fdfea1e0a007c056aeade8d76f26ee37865c3
MuRing <11esh@naver.com>
2022-01-26 22:08:07 +0900

    DynamicProgramming

--- Chp8_DynamicProgramming/MakingOne.c
+++ Chp8_DynamicProgramming/MakingOne.c
@@ -12,17 +12,30 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int x, count = 0;
     scanf("%d", &x);
 
-    int* arr = (int*)calloc(x, sizeof(int));
+    // 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+    int* dp = (int*)calloc(30001, sizeof(int));
 
     for(int i = 2; i < x + 1; i++){
-        arr[i] = arr[i - 1] + 1;
+        // 현재의 수에서 1을 빼는 경우
+        dp[i] = dp[i - 1] + 1;
 
+        // 현재의 수가 2로 나누어 떨어지는 경우
         if(i % 2 == 0)
-            arr[i] = 
+            dp[i] = dp[i] < dp[i / 2] + 1 ? dp[i] : dp[i / 2] + 1;
+
+        // 현재의 수가 3으로 나누어 떨어지는 경우
+        if(i % 3 == 0)
+            dp[i] = dp[i] < dp[i / 3] + 1 ? dp[i] : dp[i / 3] + 1;
+
+        // 현재의 수가 5로 나누어 떨어지는 경우
+        if(i % 5 == 0)
+            dp[i] = dp[i] < dp[i / 5] + 1 ? dp[i] : dp[i / 5] + 1;
     }
+    printf("%d", dp[x]);
 }
--- Chp9_ShortestPath/Dijkstra.c
+++ Chp9_ShortestPath/Dijkstra.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main(){
     int n, m;
-
+    
     // 노드의 개수, 간선의 개수 입력받기
     scanf("%d %d", &n, &m);
 
     //시작 노드의 번호 입력받기
     int start;
-    start = scanf("%d", &start);
+    scanf("%d", &start);
     printf("start : %d", start);
 
+    int** graph = (int**)malloc(sizeof(int) * n + 1);
+    for(int i = 0; i < n; i++)
+        graph[i] = (int*)malloc(sizeof(int) * m);
+
+    
+    
 }
